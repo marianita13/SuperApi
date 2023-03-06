2ba8b1e57522ee87f8851f495e938d25b618e8c5
marianita13 <marianitacero.1913@gmail.com>
2023-03-05 23:10:16 -0500

    supers

--- super.js
+++ super.js
@@ -1,14 +1,22 @@
 const url="https://www.superheroapi.com/api.php/10220996770587654/search/"
+var contenido=''
 
 async function encontrar(){
-    const name = document.querySelector('#nombre').value
-    console.log(name);
+    let nombre=document.getElementById('nombre')
+    let name=nombre.value
     const respuesta=await fetch(url+name)
     const datos=await respuesta.json()
-    let superhero=document.getElementById('superhero')
-    for (i=0;i<6;i++){
-        let imagen=datos.results[i].image.url
-        console.log(imagen);
+    console.log(datos);
+    if (name==''){
+        alert('Tienes que poner un superheroe')
+    }else if (datos.response=='error'){
+        alert('Ese superheroe no existe')
+    }else{
+        let imagen=datos.results[0].image.url
+        let superhero=document.getElementById('superhero')
         superhero.innerHTML=`<img src=${imagen}>`
+        let names=datos.results[0].name
+        let fullName=datos.results[0].biography.full-name
+        let poderes=datos.results[0].powerstats
     }
 }
